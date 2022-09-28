78d97bcb20b2d606f44a58b31be21b2912411573
alardizabalsha <113388146+alardizabalsha@users.noreply.github.com>
2022-09-28 12:29:56 -0700

    working on map and engine

--- main.ts
+++ main.ts
@@ -1,40 +1,49 @@
 namespace SpriteKind {
     export const map = SpriteKind.create()
+    export const rocketengine = SpriteKind.create()
 }
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.darkGroundNorthEast1, function (sprite, location) {
     mySprite.setVelocity(0, -1)
 })
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.ay = -25
+})
 let mySprite: Sprite = null
+let angle = 0
 tiles.setCurrentTilemap(tilemap`level1`)
 effects.starField.startScreenEffect()
 mySprite = sprites.create(img`
-    ....ffffff.........ccc..
-    ....ff22ccf.......cc4f..
-    .....ffccccfff...cc44f..
-    ....cc24442222cccc442f..
-    ...c9b4422222222cc422f..
-    ..c999b2222222222222fc..
-    .c2b99111b222222222c22c.
-    c222b111992222ccccccc22f
-    f222222222222c222ccfffff
-    .f2222222222442222f.....
-    ..ff2222222cf442222f....
-    ....ffffffffff442222c...
-    .........f2cfffc2222c...
-    .........fcc2ffffffff...
-    ..........fc2ffff.......
-    ...........fffff........
+    . . . . . . . e c 7 . . . . . . 
+    . . . . e e e c 7 7 e e . . . . 
+    . . c e e e e c 7 e 2 2 e e . . 
+    . c e e e e e c 6 e e 2 2 2 e . 
+    . c e e e 2 e c c 2 4 5 4 2 e . 
+    c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
+    . e e e 2 2 2 2 2 2 2 2 2 4 e . 
+    . 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
+    . . 2 e e 2 2 2 2 2 4 4 2 e . . 
+    . . . 2 2 e e 4 4 4 2 e e . . . 
+    . . . . . 2 2 e e e e . . . . . 
     `, SpriteKind.Player)
+let mySprite3 = sprites.create(img`
+    5 5 
+    7 7 
+    `, SpriteKind.rocketengine)
 scene.cameraFollowSprite(mySprite)
 scaling.scaleByPercent(mySprite, -25, ScaleDirection.Uniformly, ScaleAnchor.Middle)
 mySprite.ay = 20
 let myMinimap = minimap.minimap(MinimapScale.Eighth, 2, 0)
 let mySprite2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.Player)
-mySprite.x = 0
 game.onUpdate(function () {
-    mySprite.destroy()
+    mySprite2.destroy()
     myMinimap = minimap.minimap(MinimapScale.Eighth, 2, 0)
     minimap.includeSprite(myMinimap, mySprite, MinimapSpriteScale.MinimapScale)
     mySprite2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.map)
     mySprite2.setPosition(mySprite.left, mySprite.top)
+    mySprite.setPosition(mySprite.x, mySprite.y)
 })
