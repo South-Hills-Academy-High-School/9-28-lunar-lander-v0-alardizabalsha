9e65b476aeb2bc2dc85c7a49aa147f9f9f44fbb3
alardizabalsha <113388146+alardizabalsha@users.noreply.github.com>
2022-09-29 14:20:06 -0700

    finished fireball

--- main.ts
+++ main.ts
@@ -8,6 +8,7 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.darkGroundNorthEast1, fun
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     apple.ay = 25 * Math.sin(angle)
     apple.ax = 25 * Math.cos(angle)
+    fireball.setFlag(SpriteFlag.Invisible, false)
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     angle += 15 * (3.14 / 180)
@@ -17,6 +18,26 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 let apple: Sprite = null
 let angle = 0
+let fireball: Sprite = null
+fireball = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 6 6 6 6 . . . . . . 
+    . . . . 6 6 6 5 5 6 6 6 . . . . 
+    . . . 7 7 7 7 6 6 6 6 6 6 . . . 
+    . . 6 7 7 7 7 8 8 8 1 1 6 6 . . 
+    . . 7 7 7 7 7 8 8 8 1 1 5 6 . . 
+    . 6 7 7 7 7 8 8 8 8 8 5 5 6 6 . 
+    . 6 7 7 7 8 8 8 6 6 6 6 5 6 6 . 
+    . 6 6 7 7 8 8 6 6 6 6 6 6 6 6 . 
+    . 6 8 7 7 8 8 6 6 6 6 6 6 6 6 . 
+    . . 6 8 7 7 8 6 6 6 6 6 8 6 . . 
+    . . 6 8 8 7 8 8 6 6 6 8 6 6 . . 
+    . . . 6 8 8 8 8 8 8 8 8 6 . . . 
+    . . . . 6 6 8 8 8 8 6 6 . . . . 
+    . . . . . . 6 6 6 6 . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Projectile)
+fireball.setFlag(SpriteFlag.Invisible, true)
 angle = 0
 tiles.setCurrentTilemap(tilemap`level1`)
 effects.starField.startScreenEffect()
@@ -54,4 +75,5 @@ game.onUpdate(function () {
     minimap2 = sprites.create(minimap.getImage(myMinimap), SpriteKind.map)
     minimap2.setPosition(apple.x - 50, apple.y - 50)
     engine.setPosition(apple.x + -8 * Math.cos(angle), apple.y + -8 * Math.sin(angle))
+    fireball.setPosition(apple.x + -8 * Math.cos(angle), apple.y + -8 * Math.sin(angle))
 })
